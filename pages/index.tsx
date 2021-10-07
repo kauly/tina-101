e7679aa8e2bbb5e844fd1e87aa62ac9a3fce7c65
kauly <kauly@hotmail.com>
2021-10-07 01:24:52 -0300

    first

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+function HomePage() {
+  return (
+    <main className="bg-blue-200 h-screen w-full flex flex-col items-center">
+      <div className="w-full p-4 text-center">
+        <h1 className="text-3xl font-bold tracking-wide block">Primeira 2006 - 14biz</h1>
+        <h3 className="text-2xl font-medium tracking-wide block">CHURRASCO</h3>
+      </div>
+      <div className="bg-gray-50 rounded p-4">
+        <h3 className="text-2xl font-medium tracking-wide block">Dia: 16/10</h3>
+        <h3 className="text-2xl font-medium tracking-wide block">Local: Rancho do Savas - </h3>
+      </div>
+
+    </main>
+  );
+}
+
+export default HomePage;
